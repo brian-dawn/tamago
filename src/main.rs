e011c18d6b0fafe0d07b37f182754b9d4b930b09
Brian Dawn <brian.t.dawn@gmail.com>
2023-07-14 08:13:40 -0500

    Add some initial CLI commands.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
 use console::Term;
 use indicatif::{ProgressBar, ProgressStyle};
 
+use std::os::linux::raw;
+use std::path::PathBuf;
 use std::process::{Command, Stdio};
 use std::{io::Cursor, path::Path, time::Duration};
 use tokio::task::JoinHandle;
 
 use anyhow::{Context, Result};
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Run {
+        /// python version to use, e.g. 3.11.3
+        version: String,
+
+        #[clap(trailing_var_arg = true, allow_hyphen_values = true)]
+        raw_remaining_args: Vec<String>,
+    },
+
+    /// build a python version from source
+    Build {
+        /// python version to build, e.g. 3.11.3
+        version: String,
+    },
+}
 
 // build relevant python versions automatically, allow fetching updates
 // act as a proxy for a python version based on the .python-version file or the pyproject.toml file.
@@ -101,24 +133,7 @@ async fn download_and_build(version: &str) -> Result<()> {
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let args: Vec<String> = std::env::args().skip(1).collect();
-
-    let version = "3.11.3";
-    download_and_build(version).await?;
-
-    let home_dir = home::home_dir().context("failed to find home dir")?;
-    let tamago_dir = home_dir.join(".tamago");
-    let sources_dir = tamago_dir.join("sources");
-
-    let build_dir = tamago_dir.join("build");
-    let install_dir = tamago_dir.join("install").join(version);
-
-    use std::process::Command;
-
-    // Assume that python_path is a string containing the path to the desired Python interpreter,
-    // and that args is a Vec<String> containing the arguments passed by the user.
+fn proxy_python(install_dir: &Path, args: &[&str]) -> Result<()> {
     let python_path = install_dir.join("bin").join("python3");
     println!("{}", python_path.display());
     // let output = Command::new(python_path)
@@ -131,23 +146,58 @@ async fn main() -> Result<()> {
     // println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
 
     let mut child = Command::new(python_path)
-        .args(&args)
+        .args(args)
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .spawn()?;
 
     let exit_status = child.wait()?;
+    std::process::exit(exit_status.code().unwrap_or(1));
+}
 
-    if exit_status.success() {
-        println!("Python interpreter exited successfully");
-    } else {
-        if let Some(code) = exit_status.code() {
-            println!("Python interpreter exited with status code {}", code);
-        } else {
-            println!("Python interpreter process terminated by signal");
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    let cli = Cli::parse();
+
+    match cli.command {
+        Some(Commands::Build { version }) => {
+            download_and_build(&version).await?;
+            return Ok(());
         }
+
+        Some(Commands::Run {
+            version,
+            raw_remaining_args,
+        }) => {
+            let home_dir = home::home_dir().context("failed to find home dir")?;
+            let install_dir = home_dir.join(".tamago").join("install").join(&version);
+
+            let args: Vec<&str> = raw_remaining_args.iter().map(AsRef::as_ref).collect();
+
+            proxy_python(&install_dir, &args)?;
+
+            return Ok(());
+        }
+        None => {}
     }
+
+    let version = "3.11.3";
+    // download_and_build(version).await?;
+
+    let home_dir = home::home_dir().context("failed to find home dir")?;
+    let tamago_dir = home_dir.join(".tamago");
+    let sources_dir = tamago_dir.join("sources");
+
+    let build_dir = tamago_dir.join("build");
+    let install_dir = tamago_dir.join("install").join(version);
+
+    // Assume that python_path is a string containing the path to the desired Python interpreter,
+    // and that args is a Vec<String> containing the arguments passed by the user.
+
+    // Exit with the same code as the Python process.
 
     Ok(())
 }
